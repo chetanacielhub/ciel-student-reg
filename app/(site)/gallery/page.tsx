254ee320a85514ae9ea7e96a30cf35de48fe0211
Noorja123 <charanianoorjahan@gmail.com>
2026-08-31 18:28:13 +0530

    //

--- app/(site)/gallery/page.tsx
+++ app/(site)/gallery/page.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
 };
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default async function GalleryPage() {
   const imagesList = await getGalleryImages();
